64d73cce7a2886a7d2e0f3b701fbd4c77226fee9
ytyaru <ytyaru@outlook.jp>
2019-05-24 15:10:05 +0900

    初回コミット

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * Rustの変数と可変性。
+ * CreatedAt: 2019-05-24
+ */
+fn main() {
+    let mut _x = 0;
+    _x = 1;
+//    let _y = 0; // error[E0384]: cannot assign twice to immutable variable `_y`
+//    _y = 1;
+}
+
